9e378d99bbcb9f216d3ae34dfe54061836cfd8aa
Forhad96 <forhadairdrop@gmail.com>
2023-11-05 08:02:34 +0600

    get all data by price range complete

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
-
-
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shhvx1o.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,46 +37,67 @@ run().catch(console.dir);
 // # Update a single document in the "users" collection by its ID
 // # Delete a single document from the "users" collection by its ID
 
-
 // Assignment 11::Database
-const database =client.db('hotelDB')
+const database = client.db("hotelDB");
 // Database::Collection
-const roomsCollection = database.collection('rooms')
-
-
+const roomsCollection = database.collection("rooms");
 
 // Get::Method
 
-//Get::All_Rooms
-app.get('/rooms',async(req,res)=>{
-  try{
-    // const review = req.query.reviews
-    const result = await roomsCollection.find().toArray()
-    res.send(result)
-  }catch(error){
-    console.log(error);
-  }
-})
-//Get::Single_Room
-app.get('/rooms/:id',async(req,res)=>{
-  try{
-    const id = req.params.id
-    console.log(typeof id);
-    const query = {_id: new ObjectId(id)}
-    const result = await roomsCollection.findOne(query)
-    res.send(result)
 
-  }catch(error){
+// app.get("/rooms", async (req, res) => {
+//   try {
+//     const result = await roomsCollection.find().toArray();
+//     res.send(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+//Get::All_Rooms
+// Filter room by price range http://localhost:3000/rooms?minPrice=200&maxPrice=400
+app.get("/rooms", async (req, res) => {
+  try {
+    const query = {}
+ const minPrice = parseFloat(req.query.minPrice); // Convert to number
+ const maxPrice = parseFloat(req.query.maxPrice); // Convert to number
+
+//  if (isNaN(minPrice) || isNaN(maxPrice)) {
+//    return res.status(400).json({ error: "Invalid price range parameters" });
+//  }
+//  else{
+//   query.price = { $gte: minPrice, $lte: maxPrice }
+  
+//  }
+if(minPrice || maxPrice){
+    query.price = { $gte: minPrice, $lte: maxPrice }
+}
+
+
+ const filteredRooms = await roomsCollection.find(query).toArray();
+
+ res.send(filteredRooms);
+  } catch (error) {
     console.log(error);
   }
-})
+});
+
+//Get::Single_Room
+app.get("/rooms/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(typeof id);
+    const query = { _id: new ObjectId(id) };
+    const result = await roomsCollection.findOne(query);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Post::Method
 // Put::Method
 // Patch::Method
 // Delete::Method
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
