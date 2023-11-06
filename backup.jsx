50c182504cc7b4abeafe1e453abd7b20b6e43a39
Forhad96 <forhadairdrop@gmail.com>
2023-11-06 19:14:08 +0600

    created api for jwt token

--- backup.jsx
+++ backup.jsx
@@ -10,12 +10,16 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173/"],
     credentials: true,
   })
 );
 app.use(express.json());
-// app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shhvx1o.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,31 +32,10 @@ const client = new MongoClient(uri, {
   },
 });
 
-// my created middle ware
-const logger = (req, res, next) => {
-  console.log("middleman information", req.method, req.url);
-  next();
-};
-
-const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized Access" });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "unauthorized Access" });
-    }
-    req.user = decoded;
-    next();
-  });
-};
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -65,37 +48,16 @@ async function run() {
 }
 run().catch(console.dir);
 
+// # Get all documents from the "users" collection
+// # Get a single document from the "users" collection by its ID
+// # Create a new document in the "users" collection
+// # Update a single document in the "users" collection by its ID
+// # Delete a single document from the "users" collection by its ID
 
-//  bookify::Database
+// Assignment 11::Database
 const database = client.db("hotelDB");
 // Database::Collection
 const roomsCollection = database.collection("rooms");
-
-    // Auth related api
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .send({ success: true });
-      // console.log({ token });
-      console.log(user);
-    });
-
-    app.post("/logout", async (req, res) => {
-      const user = req.body;
-      // console.log(user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
-    });
-
-
-// DataBase Related api
 
 // Get::Method
 
@@ -139,6 +101,24 @@ app.get("/rooms/:id", async (req, res) => {
   }
 });
 
+// Post::Method
+
+// jwt token routes
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .send({ success: true });
+  // console.log({ token });
+  console.log(user);
+});
 
 // add new review api
 app.post("/rooms/:reviewId", async (req, res) => {
@@ -161,13 +141,10 @@ app.post("/rooms/:reviewId", async (req, res) => {
 // Patch::Method
 // Delete::Method
 
-
-
-
 app.get("/", (req, res) => {
-  res.send("bookify is running");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`bookify Server is running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
